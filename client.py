4c3c7c7988e46a8b239b586856eaeada4b5291ff
Eric Mesquita <ericmesqui2468@gmail.com>
2023-11-28 13:20:33 -0300

    multual messages supported and client can view them now

--- client.py
+++ client.py
@@ -1,24 +1,43 @@
 import sys
+import threading
 import CORBA, ChatApp
 
-orb = CORBA.ORB_init(sys.argv)
-ior = input("Enter IOR: ")
-obj = orb.string_to_object(ior)
-server = obj._narrow(ChatApp.ChatServer)
 
-# Join the chat
-name = input("Enter your name: ")
-server.joinChat(name)
+# function to receive the messages from the server
+def receive_messages():
+    last_received_message = None
+    while True:
+        messages = server.getMessages()
+        if messages:
+            newest_message = messages[-1]
+            if last_received_message != newest_message:
+                last_received_message = newest_message
+                print(newest_message)
 
-while True:
-    # Send a message
-    message = input("Enter a message (or 'quit' to quit): ")
-    if message.lower() == 'quit':
-        server.quitChat(name)
-        break
-    server.sendMessage(message, name)
+# function to send the messages to the server
+def send_messages():
+    while True:
+        # Send a message
+        message = input("Enter a message (or '-q' to quit): \n")
+        if message.lower() == '-q':
+            server.quitChat(name)
+            break
+        server.sendMessage(message, name)
 
-    # Retrieve and print messages
-    messages = server.getMessages()
-    for message in messages:
-        print(message)
+
+if __name__ == '__main__':
+    orb = CORBA.ORB_init(sys.argv)
+    ior = input("Enter IOR: ")
+    obj = orb.string_to_object(ior)
+    server = obj._narrow(ChatApp.ChatServer)
+
+    # Join the chat
+    name = input("Enter your name: ")
+    server.joinChat(name)
+    print(f"{name} has joined the chat")
+
+    # Start a thread to check for messages
+    threading.Thread(target=receive_messages, daemon=True).start()
+
+    # Start a thread to send messages
+    threading.Thread(target=send_messages).start()
--- server.py
+++ server.py
@@ -1,6 +1,12 @@
 import sys
+import datetime
+import os
+import logging
 import CORBA, ChatApp, ChatApp__POA
 
+# Set up logging and log the start of the server
+logging.basicConfig(filename='chat.log', level=logging.INFO)
+logging.info('Server started at ' + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
 class ChatServer_i(ChatApp__POA.ChatServer):
     def __init__(self):
@@ -8,25 +14,36 @@ class ChatServer_i(ChatApp__POA.ChatServer):
         self.messages = []
 
     def sendMessage(self, message, sender):
-        self.messages.append(f"{sender}: {message}")
-        print(f"{sender}: {message}")
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        log_message = f"{timestamp} - {sender}: {message}"
+        self.messages.append(log_message)
+        logging.info(log_message)  # Log the message
+        print(log_message)
         for client in self.clients:
             client.receiveMessage(message, sender)
 
     def receiveMessage(self):
-        # Implementation as needed
-        return ""
+        #for each new message its loaded this function is called to get the message on the client side
+        return "paia"
 
     def joinChat(self, name):
-        self.messages.append(f"{name} has joined the chat")
-        print(f"{name} has joined the chat")
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        join_message = f"{timestamp} - {name} has joined the chat"
+        self.messages.append(join_message)
+        logging.info(join_message)  # Log the join event
+        print(join_message)
 
     def quitChat(self, name):
-        self.messages.append(f"{name} has left the chat")
-        print(f"{name} has left the chat")
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        quit_message = f"{timestamp} - {name} has left the chat"
+        self.messages.append(quit_message)
+        logging.info(quit_message)  # Log the quit event
+        print(quit_message)
 
     def getMessages(self):
         return self.messages
+
+
 
 
 orb = CORBA.ORB_init(sys.argv)
